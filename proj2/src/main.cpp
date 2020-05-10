ee4a2031f987622cb67691eb3ed7cd2874521014
David Duque <david.f.s.duque@tecnico.ulisboa.pt>
2020-05-10 15:46:01 +0000

    Add file

---
+++ proj2/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+    ASA 19/20, 2nd project
+
+    David Ferreira de Sousa Duque, nº 93698
+    Ricardo Jorge Santos Subtil. nº 93752
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << argc << endl;
+    cout << argv << endl;
+    exit(EXIT_SUCCESS);
+}
